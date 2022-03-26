08b10fdb7f2a3c2d8a87d42a2f8dd34a4c7d6962
Vinny <vinny.sajja@gmail.com>
2022-03-26 09:38:01 -0600

    first test

---
+++ src/test/java/restassured/example/SigFigLoginAPITest.java
@@ -0,0 +1,19 @@
+package restassured.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Example test suite for the Login API on SigFig.com
+ * using restassured.
+ *
+ * @author vsajja
+ */
+public class SigFigLoginAPITest {
+
+    @Test
+    public void firstTest() {
+        System.out.println("first test");
+    }
+}
+
