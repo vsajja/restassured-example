eb1a5a4a5b008136d49275ced20368263384c396
Vinny <vinny.sajja@gmail.com>
2022-03-26 17:56:46 -0600

    first POST login test - happy path

--- src/test/java/restassured/example/SigFigLoginAPITest.java
+++ src/test/java/restassured/example/SigFigLoginAPITest.java
@@ -19,11 +19,35 @@ public class SigFigLoginAPITest {
         baseURI = "https://www.sigfig.com/l/account/api";
 
         get("/info").
-                then().
-                    statusCode(200).
-                    body("info.username", equalTo(null)).
-                    body("info.isAuthenticated", equalTo(false)).
-                log().all();
+        then().
+            statusCode(200).
+            body("info.username", equalTo(null)).
+            body("info.isAuthenticated", equalTo(false)).
+        log().all();
     }
+
+    @Test
+    public void testLogin() {
+        baseURI = "https://www.sigfig.com/l/v1/account/api/";
+        String SIFIG_LOGIN_VALID_USERNAME = "PlatformAutomationAssignment0107";
+        String SIFIG_LOGIN_VALID_PASSWORD = "Automation1234$";
+
+        given().
+                header("user-agent", "Mozilla/5.0").
+                header("authority", "www.sigfig.com").
+                contentType("application/x-www-form-urlencoded; charset=utf-8").
+                formParam("userName", SIFIG_LOGIN_VALID_USERNAME).
+                formParam("password", SIFIG_LOGIN_VALID_PASSWORD).
+                formParam("verificationCode", "").
+                formParam("userRemember", false).
+                accept("application/json, text/plain, */*").
+        when().
+                post("/login").
+        then().
+                body("success.user.username", equalTo(SIFIG_LOGIN_VALID_USERNAME)).
+                body("success.user.isAuthenticated", equalTo(true)).
+                statusCode(200).log().all();
+    }
+
 }
 
