770d1f4d269e466ff4cd31661390aedb1362a76a
Vinny <vinny.sajja@gmail.com>
2022-03-26 18:59:47 -0600

    more login tests

--- src/test/java/restassured/example/SigFigLoginAPITest.java
+++ src/test/java/restassured/example/SigFigLoginAPITest.java
@@ -1,53 +1,92 @@
 package restassured.example;
 
-import org.junit.Assert;
+import org.apache.http.entity.ContentType;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 /**
- * Example test suite for the Login API on SigFig.com
- * using restassured.
+ * Simple test suite for the SigFig.com Login API
+ * using rest-assured.
  *
  * @author vsajja
  */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SigFigLoginAPITest {
-    @Test
-    public void testGetAccountInfo() {
-        baseURI = "https://www.sigfig.com/l/account/api";
+    public static String SIFIG_LOGIN_VALID_USERNAME = "PlatformAutomationAssignment0107";
+    public static String SIFIG_LOGIN_VALID_PASSWORD = "Automation1234$";
 
-        get("/info").
+    @BeforeClass
+    public static void setUpClass() {
+        baseURI = "https://www.sigfig.com/l/v1/account/api/";
+    }
+
+    @Test
+    public void testLogin_InvalidUsernamePassword() {
+        given().
+            contentType(ContentType.APPLICATION_FORM_URLENCODED.getMimeType()).
+            accept(ContentType.APPLICATION_JSON.getMimeType()).
+            formParam("userName", SIFIG_LOGIN_VALID_USERNAME).
+            formParam("password", "invalid_password").
+        when().
+            post("/login").
         then().
             statusCode(200).
-            body("info.username", equalTo(null)).
-            body("info.isAuthenticated", equalTo(false)).
+            body("error._", containsString("The username and password you entered " +
+                    "don't match our records.")).
         log().all();
     }
 
     @Test
-    public void testLogin() {
-        baseURI = "https://www.sigfig.com/l/v1/account/api/";
-        String SIFIG_LOGIN_VALID_USERNAME = "PlatformAutomationAssignment0107";
-        String SIFIG_LOGIN_VALID_PASSWORD = "Automation1234$";
-
+    public void testLogin_MissingPassword() {
         given().
-                header("user-agent", "Mozilla/5.0").
-                header("authority", "www.sigfig.com").
-                contentType("application/x-www-form-urlencoded; charset=utf-8").
-                formParam("userName", SIFIG_LOGIN_VALID_USERNAME).
-                formParam("password", SIFIG_LOGIN_VALID_PASSWORD).
-                formParam("verificationCode", "").
-                formParam("userRemember", false).
-                accept("application/json, text/plain, */*").
+            contentType(ContentType.APPLICATION_FORM_URLENCODED.getMimeType()).
+            accept(ContentType.APPLICATION_JSON.getMimeType()).
+            formParam("userName", SIFIG_LOGIN_VALID_USERNAME).
         when().
-                post("/login").
+            post("/login").
         then().
-                body("success.user.username", equalTo(SIFIG_LOGIN_VALID_USERNAME)).
-                body("success.user.isAuthenticated", equalTo(true)).
-                statusCode(200).log().all();
+            statusCode(200).
+            body("error.username", equalTo(null)).
+            body("error.password", equalTo("Required")).
+        log().all();
     }
 
-}
+    @Test
+    public void testLogin_MissingUsernameOrPassword() {
+        given().
+            contentType(ContentType.APPLICATION_FORM_URLENCODED.getMimeType()).
+            accept(ContentType.APPLICATION_JSON.getMimeType()).
+        when().
+            post("/login").
+        then().
+            statusCode(200).
+            body("error.userName", equalTo("Required")).
+            body("error.password", equalTo("Required")).
+        log().all();
+    }
 
+    @Test
+    public void testLogin_Valid() {
+        given().
+            contentType(ContentType.APPLICATION_FORM_URLENCODED.getMimeType()).
+            accept(ContentType.APPLICATION_JSON.getMimeType()).
+            accept(ContentType.APPLICATION_JSON.getMimeType()).
+            formParam("userName", SIFIG_LOGIN_VALID_USERNAME).
+            formParam("password", SIFIG_LOGIN_VALID_PASSWORD).
+            formParam("verificationCode", "").
+            formParam("userRemember", false).
+        when().
+            post("/login").
+        then().
+            statusCode(200).
+            body("success.user.username", equalTo(SIFIG_LOGIN_VALID_USERNAME)).
+            body("success.user.isAuthenticated", equalTo(true)).
+            cookie("nv_ps_0"). // checks if the nv_ps_01session cookie is set in the response
+        log().all();
+    }
+}
